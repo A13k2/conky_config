e20978fa1de76cdfaf4e2c96dc2cc7f04a704a7a
A13k2 <alexander.drossel@protonmail.com>
2018-12-31 13:38:17 +0100

    awk rulez switched from python script to awk for efficency

--- viewnotes.sh
+++ viewnotes.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-#tasks=$(task export)
-#echo $tasks
-#echo $tasks | awk 'BEGIN{OFS="\t\t"; FS=":|,"; RS="}, {"; print "ID \t\t Task \t\t Urgency"}{print $2, $4, $16}' | sed 's/[]["}]//g'
-task export > ~/conky_config/task.json
-python ~/conky_config/format_notes.py
+tasks=$(task export)
+echo $tasks | awk -f format_notes.awk | sed 's/[]["\\]//g'
